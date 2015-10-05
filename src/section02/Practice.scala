8a29c374ddf53d2ce8827e31532a4367755e24ab
DaesapWillsm <daesapwillsm@gmail.com>
2015-10-05 16:35:57 +0900

    ...

--- src/section02/Practice.scala
+++ src/section02/Practice.scala
@@ -1,8 +1,5 @@
 package section02
 
-/**
- * Created by Daesap on 2015-10-02.
- */
 object Practice {
 
 	// Practice 2-1.
@@ -15,4 +12,29 @@ object Practice {
 	}
 
 	// Practice 2-2.
+	def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+		@annotation.tailrec
+		def go(n: Int): Boolean = {
+			if (n >= as.length - 1) true
+			else if (ordered(as(n), as(n + 1)) == false) false
+			else go(n + 1)
+		}
+
+		go(0)
+	}
+
+	// Practice 2-3.
+	def curry[A,B,C](f: (A,B) => C): A => B => C = {
+		a => b => f(a,b)
+	}
+
+	// Practice 2-4.
+	def uncurry[A,B,C](f: A => B => C): (A,B) => C = {
+		(a,b) => f(a)(b)
+	}
+
+	// Practice 2-5.
+	def compose[A,B,C](f: B => C, g: A => B): A => C = {
+		a => f(g(a))
+	}
 }
--- src/section02/test.sc
+++ src/section02/test.sc
@@ -1,11 +1,22 @@
 import section02._
 
+// example.
 Example.factorial(1)
 Example.factorial(10)
 
+// test 2-1.
 Practice.fibonacci(1)
 Practice.fibonacci(10)
 
+// test 2-2.
+Practice.isSorted(Array(1,2,3,4,5), (a: Int, b: Int ) => a < b)
 
+// test 2-3.
+Practice.curry((a: Int, b: Int) => a+b)(1)(2)
 
+// test 2-4.
+Practice.uncurry((a: Int) => (b: Int) => a + b)(1,2)
+
+// test 2-5.
+Practice.compose((b: Int) => b + 1, (a: Int) => a + 1)(1)
 
