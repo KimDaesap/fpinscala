e470e28424ec43b5977f96bd7ae28abc969a25d3
DaesapWillsm <daesapwillsm@gmail.com>
2015-10-05 17:25:41 +0900

    ...

---
+++ src/section03/List.scala
@@ -0,0 +1,25 @@
+package section03
+
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+
+	def sum(ints: List[Int]): Int = ints match {
+		case Nil => 0
+		case Cons(x, xs) => x + sum(xs)
+	}
+
+	def product(ds: List[Double]): Double = ds match {
+		case Nil => 1.0
+		case Cons(0.0, _) => 0.0
+		case Cons(x,xs) => x * product(xs)
+	}
+
+	def apply[A](as: A*): List[A] = {
+		if (as.isEmpty) Nil
+		else Cons(as.head, apply(as.tail: _*))
+	}
+
+}
---
+++ src/section03/test.sc
@@ -0,0 +1,10 @@
+import section03._
+
+List(1,2,3)
+Cons(1, List(2,3,4))
+List.sum(List(1,2,3))
+
+
+
+
+
