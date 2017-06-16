0e7000500e6d4db07f540e899dc7d5d11df11327
Choi sangmin <sapzil78jw@gmail.com>
2017-06-16 17:50:33 +0900

    # Section 04 working - exercise 4-8 문제 풀이 진행중

--- src/section04/Either.scala
+++ src/section04/Either.scala
@@ -4,17 +4,17 @@ sealed trait Either[+E, +A] {
   /* EXERCISE 4-6 */
   // Right 값에 대해 작용하는 버전의 map, flatMap, orElse, map2, Either를 구현하라.
   def map[B](f: A => B): Either[E, B] = this match {
-    case Right(v) => Right(f(v))
+    case Right(a) => Right(f(a))
     case Left(e) => Left(e)
   }
 
   def flatMap[EE >: E,B](f: A => Either[EE, B]): Either[EE, B] = this match {
-    case Right(v) => f(v)
+    case Right(a) => f(a)
     case Left(e) => Left(e)
   }
 
   def orElse[EE >: E, B >: A](b: Either[EE, B]): Either[EE, B] = this match {
-    case Right(v) => this
+    case Right(_) => this
     case _ => b
   }
 
@@ -27,23 +27,27 @@ case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends  Either[Nothing, A]
 
 
-object Eigher {
+object Either {
   /* EXERCISE 4-7 */
   // Either에 대한 sequence와 traverse를 작성하라.
   // 이 두 함수는 발생한 첫 오류를 돌려주어야 한다 (오류가 발생했다면)
-  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = es match {
-    case Nil => Right(Nil)
-    case h :: t => h.flatMap(v => sequence(t).map(v :: _))
-  }
-
   def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = {
     as match {
       case Nil => Right(Nil)
-      case h  :: t => f(h).flatMap(v => traverse(t)(f).map(v :: _))
+      case h :: t => f(h).flatMap(a => traverse(t)(f).map(a :: _))
     }
   }
 
-  // page 78.
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = {
+    //    es match {
+    //      case Nil => Right(Nil)
+    //      case h :: t => h.flatMap(v => sequence(t).map(v :: _))
+    //    }
+
+    traverse(es)(a => a)
+  }
+
+  // page 78, 목록 4.4
   sealed class Name(val value: String)
   sealed class Age(val value: Int)
   case class Person(name: Name, age: Age)
@@ -61,9 +65,30 @@ object Eigher {
 
   /* EXERCISE 4-8 */
   // map2는 오류를 하나만 보고할 수 있다. 두 오류를 모두 보고하게 하려면 어떻게 해야할까?
-  // map2를 바구는 것이 좋을까. 아니면 mkPerson 서명을 바꾸는 것이 좋을까?
+  // map2를 바꾸는 것이 좋을까. 아니면 mkPerson 서명을 바꾸는 것이 좋을까?
   // 아니면 이러한 요구사항을 Either보다 더 잘 만족하는 새로운 자료 형식을 만들 수도 있을 것이다.
   // 그러한 자료 형식에 대해 orElse, tarverse, sequence는 다르게 행동할까?
+
+  // 1. map2를 바꿔보자... 구리다.
+  def map2[E, A, B, C](a: Either[E, A], b: Either[E, B])
+                      (f: (A, B) => C): Either[List[E], C] = {
+    (a, b) match {
+      case (Right(aa), Right(bb)) => Right(f(aa, bb))
+      case (a1, a2) => Left(List(a1, a2).foldLeft(List[E]()) {
+        (acc, x) => x match {
+          case Left(e) => e :: acc
+          case _ => acc
+        }
+      })
+    }
+  }
+
+  // 2. mkPerson의 서명을 바꿔보자...
+  def mkPerson1(name: String, age: Int): Either[List[String], Person] = {
+    map2(mkName(name), mkAge(age))(Person(_, _))
+  }
+
+  // 3. Either보다 더 잘 만족하는 새로운 자료형식을 만들어 보자.
 
 
 }
--- src/section04/_test.sc
+++ src/section04/_test.sc
@@ -1,41 +1,67 @@
 import section04._
 
-val some = Some(10)
-val none = None
+//val some = Some(10)
+//val none = None
+//
+//some.getOrElse(1)
+//none.getOrElse(5)
+//
+//some.flatMap(x => Some(x + 1))
+//
+//val seq = Seq(1.0, 2.0, 3.0, 4.0, 5.0)
+//val mean = seq.sum / seq.length
+//
+//Option.variance(Seq())
+//Option.variance(seq)
+//Option.lift(math.abs)(Some(-1))
+//
+//val a = Some(1)
+//val b = None
+//def f(a: Int, b: Int): Int = {
+//  a + b
+//}
+//
+//a.flatMap(aa => b.map(bb => f(aa, bb)))
+//
+//Option.map2(Some(1), Some(2))(f)
+//
+//Option.sequence(List(Some(1), Some(2), Some(3)))
+//
+//Option.traverse(List("1", "2", "3" , "4", "5"))(x => Option.Try(x.toInt))
+//
+//val eigherA = Right(10)
+//val eigherB = Right(5)
+//val eigherC = Left("I am C")
+//
+//eigherA.flatMap(x => Right(x * 10))
+//eigherA.map2(Right("I am B"))((a,b) => Left("I am Right"))
+//eigherB.map2(eigherA)((a,b) => Left("I am Right"))
+//eigherB.map2(eigherC)((a,b) => Left("I am Right"))
 
-some.getOrElse(1)
-none.getOrElse(5)
+val name = Either.mkName("")
+val age = Either.mkAge(-1)
 
-some.flatMap(x => Some(x + 1))
+List(name, age).filter(_.isInstanceOf[Left[String]])
 
-val seq = Seq(1.0, 2.0, 3.0, 4.0, 5.0)
-val mean = seq.sum / seq.length
+Either.mkPerson1("Kim daesap", 10)
+Either.mkPerson1("Kim daesap", -1)
+Either.mkPerson1("", 10)
+Either.mkPerson1("", -1)
 
-Option.variance(Seq())
-Option.variance(seq)
-Option.lift(math.abs)(Some(-1))
 
-val a = Some(1)
-val b = None
-def f(a: Int, b: Int): Int = {
-  a + b
-}
 
-a.flatMap(aa => b.map(bb => f(aa, bb)))
 
-Option.map2(Some(1), Some(2))(f)
 
-Option.sequence(List(Some(1), Some(2), Some(3)))
 
-Option.traverse(List("1", "2", "3" , "4", "5"))(x => Option.Try(x.toInt))
 
-val eigherA = Right(10)
-val eigherB = Right(5)
-val eigherC = Left("I am C")
 
-eigherA.flatMap(x => Right(x * 10))
-eigherA.map2(Right("I am B"))((a,b) => Left("I am Right"))
-eigherB.map2(eigherA)((a,b) => Left("I am Right"))
-eigherB.map2(eigherC)((a,b) => Left("I am Right"))
+
+
+
+
+
+
+
+
 
 
