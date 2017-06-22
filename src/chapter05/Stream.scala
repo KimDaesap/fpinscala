d58cc2bcc3a2303f90954d1412a6b3dd679728c8
Choi sangmin <sapzil78jw@gmail.com>
2017-06-22 11:28:17 +0900

    # Chapter 05 - exercise 5-1 ~ 5-3 문제 풀이 완료

--- src/chapter05/Stream.scala
+++ src/chapter05/Stream.scala
@@ -1,4 +1,11 @@
+package chapter05
+
 sealed trait Stream[+A] {
+  // 디버깅용 출력 함수
+  def print: String = this match {
+    case Cons(h, t) => h().toString + ", " + t().print
+    case _ => "End"
+  }
 
   def headOption: Option[A] = this match {
     case Empty => None
@@ -7,14 +14,29 @@ sealed trait Stream[+A] {
 
   /* EXERCISE 5-1 */
   // toList 함수를 작성하라
-  def toList: List[A] = ???
+  def toList: List[A] = this match {
+    case Empty => Nil
+    case Cons(h, t) => h() :: t().toList
+  }
 
   /* EXERCISE 5-2 */
   // take(n)과 drop(n) 함수를 작성하라
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 0 => Cons(h, () => t().take(n - 1))
+    case _ => Empty
+  }
+
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 0 => t().drop(n -1)
+    case _ => this
+  }
 
   /* EXERCISE 5-3 */
   // takeWhile 함수를 작성하라.
-  def takeWhile(p: A=>Boolean): Stream[A] = ???
+  def takeWhile(p: A=>Boolean): Stream[A] = this match {
+    case Cons(h, t) if p(h()) => Cons(h, () => t().takeWhile(p))
+    case _ => Empty
+  }
 
 }
 
@@ -39,7 +61,6 @@ object Stream {
   // 참고 :
   //   http://docs.scala-lang.org/ko/tutorials/tour/extractor-objects.html
   //   https://blog.outsider.ne.kr/502
-
   def apply[A](as: A*): Stream[A] = {
     if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
--- src/chapter05/test05.sc
+++
@@ -1,7 +0,0 @@
-import chapter05._
-
-val s = Stream(1,2,3)
-
-Stream()
-
-
